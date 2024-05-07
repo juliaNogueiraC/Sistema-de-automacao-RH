317a5dfb3d4d7adcf3eba2eeb6b894a4b69f47eb
Júlia Nogueira <69528739+juliaNogueiraC@users.noreply.github.com>
2024-05-07 17:07:37 +0000

    alterações no código

--- main.py
+++ main.py
@@ -3,19 +3,40 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Autenticar utilizando o arquivo JSON de credenciais
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('seu caminho json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('caminho da pasta json', scope)
 client = gspread.authorize(creds)
 
 # Abrir a planilha pelo seu ID
-planilha = client.open_by_key('id da planilha')
+planilha = client.open_by_key('id_da_planilha')
 
 # Selecionar uma aba específica
 aba = planilha.worksheet('aba da planilha')
 
-# Função para adicionar uma nova linha com os dados especificados
-def adicionar_dados(telefone, nome, cargo, regiao, status):
-    nova_linha = [telefone, nome, cargo, regiao, status]
-    aba.append_row(nova_linha)
+def adicionar_dados(dados):
+    for linha in dados:
+        aba.append_row(linha)
+
+# Dados dos usuários fictícios
+dados_usuarios = [
+    ['98765432', 'Maria Souza', 'Analista de Marketing', 'Rio de Janeiro', 'Presente'],
+    ['87654321', 'João Santos', 'Engenheiro Civil', 'São Paulo', 'Presente'],
+    ['76543210', 'Ana Oliveira', 'Contadora', 'Brasília', 'Ausente'],
+    ['65432109', 'Luiz Pereira', 'Desenvolvedor de Software', 'Belo Horizonte', 'Presente'],
+    ['54321098', 'Carla Ferreira', 'Gerente de Vendas', 'Recife', 'Ausente'],
+    ['43210987', 'Rafael Costa', 'Médico', 'Porto Alegre', 'Presente'],
+    ['32109876', 'Juliana Lima', 'Advogada', 'Curitiba', 'Presente'],
+    ['21098765', 'Lucas Vieira', 'Estudante', 'Florianópolis', 'Ausente'],
+    ['10987654', 'Fernanda Almeida', 'Arquiteta', 'Salvador', 'Ausente'],
+    ['09876543', 'Ricardo Santos', 'Chef de Cozinha', 'Fortaleza', 'Presente'],
+    ['98765433', 'Amanda Silva', 'Jornalista', 'Rio de Janeiro', 'Presente'],
+    ['87654322', 'André Costa', 'Fotógrafo', 'São Paulo', 'Ausente'],
+    ['76543211', 'Mariana Oliveira', 'Analista de Recursos Humanos', 'Brasília', 'Presente'],
+    ['65432100', 'Gabriel Pereira', 'Professor', 'Belo Horizonte', 'Presente'],
+    ['54321090', 'Camila Ferreira', 'Farmacêutica', 'Recife', 'Ausente']
+]
+
+# Adicionando os dados dos usuários fictícios
+adicionar_dados(dados_usuarios)
 
 # Função para deletar uma linha com base no nome do usuário
 def deletar_por_nome(nome):
@@ -25,7 +46,8 @@ def deletar_por_nome(nome):
             aba.delete_rows(i + 1)  # Deleta a linha (índice começa em 1)
 
 # Exemplo de deletar uma linha pelo nome do usuário
-deletar_por_nome("")
+deletar_por_nome("nome")
 
 # Exemplo de adicionar dados
-adicionar_dados('123456789', 'Lucas Silva', 'Analista', 'São Paulo', 'Ativo')
+#adicionar_dados('1234', 'pimba', 'vigia', 'piaui', 'ausente')
+
